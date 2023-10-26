fc2d594c06c16538325dc8d76b0713ecf4aaa296
tranvanthanh99 <thanhvantran99@gmail.com>
2023-10-26 17:48:22 +0700

    feat: :sparkles: medium two sum sorted array with two pointers - rs

--- 2.1-two-pointers/medium-two-sum-ii-input-array-is-sorted-rs/src/lib.rs
+++ 2.1-two-pointers/medium-two-sum-ii-input-array-is-sorted-rs/src/lib.rs
@@ -1,12 +1,14 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 pub struct Solution {}
 
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        Self::solution_v1(numbers, target)
+        // Self::solution_v1(numbers, target)
+        Self::solution_v2(numbers, target)
     }
 
+    // hash map runtime: 3ms, memory: 2.4MB
     fn solution_v1(numbers: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::<i32, usize>::new();
 
@@ -22,5 +24,22 @@ impl Solution {
         }
 
         vec![]
+    }
+
+    // two pointer runtime: 1ms, memory: 2.2MB
+    fn solution_v2(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let (mut l, mut r) = (0, numbers.len() - 1);
+
+        while l < r {
+            if numbers[l] + numbers[r] < target {
+                l += 1;
+            } else if numbers[l] + numbers[r] > target {
+                r -= 1;
+            } else {
+                return vec![l as i32 + 1, r as i32 + 1];
+            }
+        }
+
+        unreachable!("invalid test case")
     }
 }
