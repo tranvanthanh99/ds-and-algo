b9bf4605487b2a3e6d915c201978af5a68d8187b
tranvanthanh99 <thanhvantran99@gmail.com>
2023-11-07 14:22:06 +0700

    feat: :sparkles: easy binary search - rs

---
+++ 3.1-binary-search/easy-binary-search-rs/src/lib.rs
@@ -0,0 +1,21 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+
+        while l <= r {
+            let m = l + (r - l) / 2;
+
+            if nums[m] > target {
+                r = m - 1;
+            } else if nums[m] < target {
+                l = m + 1;
+            } else {
+                return m as i32;
+            }
+        }
+        -1i32
+    }
+}
